d437f23e665c72ae760c1260e525c0be1e493c24
PedroAssencao <115711899+PedroAssencao@users.noreply.github.com>
2023-04-02 08:28:23 -0300

    CardapioV2 2/4

---
+++ Cardapio2/HomePage/script.js
@@ -0,0 +1,10 @@
+function carregaPagina(url) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("container").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
