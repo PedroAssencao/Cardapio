25f5a983a0b0df7ae7e2f468f3ef6d66d324c53d
Nitierk <115711899+PedroAssencao@users.noreply.github.com>
2023-04-27 13:04:30 -0300

    27/04

---
+++ aaa/CárdapioV3_Tunado/CárdapioV3_Tunado/Models/Produto.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace CárdapioV3_Tunado.Models
+{
+    public class Produto
+    {
+        public int ProID { get; set; }
+        public string NomePruduto { get; set; }
+        public string DetalhesProduto { get; set; }
+        public string NutricaoProduto { get; set; }
+        public double PrecoProduto { get; set; }
+        public string CategoriaProduto { get; set; }
+    }
+}
