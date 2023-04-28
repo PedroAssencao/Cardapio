734cac452b5152f1d782027212e80368d00ce7ca
PedroAssencao <115711899+PedroAssencao@users.noreply.github.com>
2023-04-27 23:09:44 -0300

    kk

---
+++ CárdapioV3_Tunado/Cardapiov3tunadomuitomaisfoda/CárdapioV3_Tunado/CárdapioV3_Tunado/Models/Empresa.cs
@@ -0,0 +1,10 @@
+﻿namespace CárdapioV3_Tunado.Models
+{
+    public class Empresa
+    {
+        public int EmpresaID { get; set; }
+        public string Telefone { get; set; }
+        public string NomeEmpresa { get; set; }
+        public string SenhaEmpresa { get; set; }
+    }
+}
