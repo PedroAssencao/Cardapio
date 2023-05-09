1e662bf68b47810717db414ff753d64a560a7999
Nitierk <115711742+Mario-Cesar1@users.noreply.github.com>
2023-05-09 17:08:19 -0300

    09/05/2023

---
+++ CárdapioV3_Tunado-20230509T171111Z-001/CárdapioV3_Tunado/CárdapioV3_Tunado/Controllers/ProdutoEmpresaController.cs
@@ -0,0 +1,61 @@
+﻿using CárdapioV3_Tunado.DAL;
+using CárdapioV3_Tunado.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace CárdapioV3_Tunado.Controllers
+{
+    [Authorize]
+    public class ProdutoEmpresaController : Controller
+    {
+        ProdutoEmpresaDAO empresa = new ProdutoEmpresaDAO();
+        public IActionResult Index()
+        {
+            var idEmpresa = int.Parse(User.Identity!.Name);
+            ViewBag.listaProdutosEmpresa = empresa.getTodosProdutosbyEmpresa(idEmpresa);
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(int ProdutoID, int CategoriaID)
+        {
+            var idEmpresa = int.Parse(User.Identity!.Name);
+            ProdutoEmpresa novoProdutoEmpresa = new ProdutoEmpresa();
+            novoProdutoEmpresa.EmpresaID = idEmpresa;
+            novoProdutoEmpresa.ProdutoID = ProdutoID;
+            novoProdutoEmpresa.CategoriaID = CategoriaID;
+            empresa.InsertProdutoEmpresa(novoProdutoEmpresa);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult ApagarProduto(string id)
+        {
+
+            ProdutoEmpresa apagarProduto = new ProdutoEmpresa();
+            apagarProduto.ProdutoID = Convert.ToInt32(id);
+            empresa.ApagarProduto(apagarProduto);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult ApagarCategoria(string id)
+        {
+
+            ProdutoEmpresa apagarCategoria = new ProdutoEmpresa();
+            apagarCategoria.CategoriaID = Convert.ToInt32(id);
+            empresa.ApagarCategoria(apagarCategoria);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CárdapioV3_Tunado-20230509T171111Z-001/CárdapioV3_Tunado/CárdapioV3_Tunado/DAL/CardapioDAO.cs
@@ -0,0 +1,92 @@
+﻿using CárdapioV3_Tunado.Models;
+using Dapper;
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+
+namespace CárdapioV3_Tunado.DAL
+{
+    public class CardapioDAO
+    {
+        private MySqlConnection _connection;
+
+        public CardapioDAO()
+        {
+            _connection = ConexaoBD.getConexao();
+        }
+
+
+        public List<CategoriaProdutoView> getTodosCategorias()
+        {
+            var sql = "select * from Categoria";
+
+            var dados = (List<CategoriaProdutoView>)_connection.Query<CategoriaProdutoView>(sql);
+            return dados;
+
+        }
+
+        public List<Empresa> getFotosEmpresa(int idEmpresa)
+        {
+            var query = "select FotoEmpresa from Empresa where EmpresaID=" + idEmpresa;
+
+            var dados = (List<Empresa>)_connection.Query<Empresa>(query);
+            return dados;
+        }
+
+
+        public List<CategoriaProdutoView> getTodosProdutosbyEmpresa(int idEmpresa)
+        {
+            var sql = "Select ep.*, p.NomeProduto, p.DescricaoProduto, p.NutricaoProduto, p.PrecoProduto, p.CategoriaProduto, p.ProID, c.Nome, c.CategoriaID, c.CategoriaDescricao, c.CategoriaFoto from produtoempresa ep join Produto p on ep.ProdutoID=p.ProID join categoria c on ep.CategoriaID=c.CategoriaID where ep.EmpresaID=" + idEmpresa;
+
+            var dados = _connection.Query<CategoriaProdutoView>(sql);
+            return (List<CategoriaProdutoView>)dados;
+        }
+
+            
+        public List<CategoriaProdutoView> getTodosProdutos()
+        {
+            var sql = "select * from Produto";
+            
+            var dados = (List<CategoriaProdutoView>)_connection.Query<CategoriaProdutoView>(sql);
+            return dados;
+        }
+
+        //inserir produtos
+
+        public void InsertProdutos(CategoriaProdutoView Produto)
+        {
+
+            string query = "insert Produto (NomeProduto,DescricaoProduto,PrecoProduto,CategoriaProduto) values (@NomeProduto,@DescricaoProduto,@PrecoProduto,@CategoriaProduto)";
+
+
+            int qtdInserida = _connection.Execute(query, Produto);
+        }
+        
+        //update
+
+        public void UptdateProdutos(CategoriaProdutoView novoProduto)
+        {
+            string query = "update Produto set NomeProduto=@NomeProduto, DescricaoProduto=@DescricaoProduto, PrecoProduto=@PrecoProduto, CategoriaProduto=@CategoriaProduto where ProID=@ProID";
+
+            int qtdAtualizada = _connection.Execute(query, novoProduto);
+
+        }
+
+        //apagar
+
+        public void ApagarProdutos(CategoriaProdutoView Produto)
+        {
+            string query = "Delete Produto where ProID=@ProID";
+
+            int qtdAtualizada = _connection.Execute(query, Produto);
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
